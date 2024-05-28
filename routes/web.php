fb03bfec220175cbf32f9b461dc0ee4442726541
asthabaj <asthabjracharya@gmail.com>
2024-05-28 15:10:39 +0545

    validation

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,38 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/hi', function () {
-    return 'Hello world';
-});
 
 Route::get('/jobs', function () {
     //$jobs= Job::with('employer')->get();//eager loading jobs along with employer for each
-    $jobs= Job::with('employer')->paginate(3);
-    return view('jobs',[
+    $jobs= Job::with('employer')->latest()->paginate(3);
+    return view('jobs.index',[
         
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
    
-    return view('job',['job' => $job]);
+    return view('jobs.show',['job' => $job]);
+});//wildcard generally bottom
+
+route::post('/jobs',function()
+{
+    request()->validate([
+        'title' => ['required','min:3'],
+        'salary' => ['required']
+    ]);
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
